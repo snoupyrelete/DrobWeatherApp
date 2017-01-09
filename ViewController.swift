c166715e8d3d82310b15d046aad88d24f2a0977e
snoupyrelete <drob8896@740s-pl-112.local>
2017-01-09 09:44:24 -0700

    Boilerplate networking code

--- ViewController.swift
+++ ViewController.swift
@@ -13,22 +13,49 @@ import SwiftyJSON
 class ViewController: UIViewController {
 
     
-    let json = JSON(data: dataFromNetworking)
-    let json = JSON(jsonObject)
-    if let dataFromString = jsonString.data(using: .utf8, allowLossyConversion: false) {
-        let json = JSON(data: dataFromString)
-    }
+  
+    var objects: [[String: String]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let myUrlStringInput: String = "http://api.wunderground.com/api/Your_Key/conditions/q/CA/San_Francisco.json"
+        //print("myJSON inputURL: \(inputURL)")
+        
+        if let nsurlStringInput = URL(string: myUrlStringInput)
+        { // We have to convert to NSURL because contentsOfUrl takes NSURL as a paramater.
+            //print("NSURL string input: \(nsurlStringInput)")
+            
+            if let websiteNSData = try? Data(contentsOf: nsurlStringInput, options: [])
+            { // We have to convert it to NSData because swiftyJSON's type JSON uses NSData as a paramter.
+                //print("Website's NSDATA: \(websiteNSData)")
+                
+                let websiteJSONData = JSON(data: websiteNSData)
+                //print("Website JSON Data: \(websiteJSONData)")
+                
+//                for article in websiteJSONData[0].arrayValue
+//                {
+//                    let weather = article["weather"].stringValue
+//                  
+//                    let obj = ["weather": weather]
+//                    
+//                    //print("obj in myJSON are  \(objects)")
+//                    //change newstables variable from here? PUT FOR LOOP IN INIT METHOD OF THIS CLASS?
+//                    self.objects.append(obj)
+//                    print(objects)
+//                    
+//                }
+              
+                print(websiteJSONData["weather"])
+            }
+        }
+        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
-
-
 }
 
