b57c24e11af54a5f2bfc1864f91bdc162eaba44c
snoupyrelete <drob8896@740s-pl-112.local>
2017-02-03 08:59:45 -0700

    Cleaned up comments

--- ViewController.swift
+++ ViewController.swift
@@ -13,10 +13,10 @@ import SwiftyJSON
 class ViewController: UIViewController, UISearchBarDelegate {
 
     @IBOutlet weak var weatherLabel: UILabel!
-    
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
+    
     // TODO: this is the endpoint for the autocomplete for the search bar : http://autocomplete.wunderground.com/aq?query=query
     // TODO: ASK CODY IF I SHOULD ACCESS THE API/JSON SERVICE AS A SINGLETON? - OR OTHER WAY? CLEANEST?
     // TODO: switch to one of wunderground's iconsets -it's easier. I like set # 11, #10, & #9.
@@ -31,11 +31,12 @@ class ViewController: UIViewController, UISearchBarDelegate {
     
     func searchBarTextDidEndEditing(_ searchBar: UISearchBar)
     {
-        // make an api call, but with the autocomplete endpoint.
+        // TODO: make an API call, but with the autocomplete REST endpoint instead.
     }
     
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
         let myUrlStringInput: String = "http://api.wunderground.com/api/4f16fc1990d1c023/forecast/geolookup/conditions/q/CA/San_Francisco.json"
@@ -53,18 +54,11 @@ class ViewController: UIViewController, UISearchBarDelegate {
                 let icon = websiteJSONData["current_observation"]["icon"].stringValue
                 let location = websiteJSONData["current_observation"]["display_location"]["full"].stringValue
 
-//                switch icon
-//                {
-//                case "Overcast":
-//                    icon = "CLOUD"
-//                default:
-//                    icon = "SUN"
-//                }
-                
+
                 let obj = ["weather": weather, "temp": temp, "icon": icon, "location": location]
+            
                 
-                
-                    self.objects.append(obj)
+                self.objects.append(obj)
           
 
             }
@@ -75,15 +69,14 @@ class ViewController: UIViewController, UISearchBarDelegate {
         locationLabel.text = objects[0]["location"]
         
         // Their assets suck - they all have white backgrounds.
-        if let url = NSURL(string: "https://icons.wxug.com/i/c/j/" + objects[0]["icon"]! + ".gif") {
-            if let data = NSData(contentsOf: url as URL) {
+        if let url = NSURL(string: "https://icons.wxug.com/i/c/j/" + objects[0]["icon"]! + ".gif")
+        {
+            if let data = NSData(contentsOf: url as URL)
+            {
                 iconImageView.image = UIImage(data: data as Data)
-            }        
+            }
         }
         
-//        iconImageView.image = UIImage(named: objects[0]["icon"]!)
     }
-
- 
 }
 
